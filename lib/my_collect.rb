a3d73836f9dd6f2567af3cf4cc6d37af7ee3d768
Robin Tram <robin.tram@gmail.com>
2018-09-20 17:56:30 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -18,3 +18,8 @@ array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
   name.split(" ").first
 end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
