8d15e59eea36e61863e6b8a2f29703d2b3f8d352
Robin Tram <robin.tram@gmail.com>
2018-09-20 17:59:43 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
+def my_collect(array)
+  if block_given?
+    i = 0 
+    new_array = []
+  
+    while i < array.length
+      new_array.push (yield array[i])
+      i += 1 
+    end 
+    
+    new_array
+  else 
+    puts "Hey! No block was given!"
+  end 
+end 
 
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
